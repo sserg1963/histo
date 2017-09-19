c274e9e35adedb8ef57ac03163e4dcd773a081a5
Sergey Sergeyev <sergey.sergeyew@gmail.com>
2017-09-19 18:13:08 +0300

    Add files via upload

---
+++ histo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define PICTURE_WIDTH 2048
+#define PICTURE_HEIGHT 2048
+#define PICTIRE_Y_DEPTH 256
+
+int main(int argc, char **argv, char **envp)
+{
+    char y[PICTURE_WIDTH][PICTURE_HEIGHT]={0};
+    int histogram_value[PICTIRE_Y_DEPTH]={0};
+    int val;
+
+    for (int h=0;h < PICTURE_HEIGHT; ++h)
+    {
+        for (int w=0; w < PICTURE_WIDTH; ++w)
+        {
+        val=y[w][h]; // set temporal value for the current pixel
+        histogram_value[val]=histogram_value[val]++; //incrementing counter of particular Y
+        }
+    }
+for (int j=0; j < PICTIRE_Y_DEPTH; ++j) printf("%d", histogram_value[j]);
+return 0;
+}
+
